e8ceaca997b114d22a5be9d08b7607b24cbda9bc
Datti0812 <83442542+Datti0812@users.noreply.github.com>
2026-01-04 06:44:11 -0800

    Add setup instructions for local environment
    Added instructions for setting up a virtual environment and installing necessary packages.

--- gradio_basics.py
+++ gradio_basics.py
@@ -1,3 +1,26 @@
+'''
+
+To run this in a local environment run the following. 
+pip install virtualenv 
+virtualenv my_env # create a virtual environment named my_env
+source my_env/bin/activate # activate my_env
+
+'''
+
+''' install these packages before you run the program 
+
+# installing necessary pacakges in my_env
+python3.11 -m pip install \
+gradio==4.44.0 \
+pydantic==2.10.6 \
+ibm-watsonx-ai==1.1.2 \
+langchain==0.2.11 \
+langchain-community==0.2.10 \
+langchain-ibm==0.1.11 \
+huggingface_hub==0.23.0 
+
+'''
+
 import gradio as gr
 
 def sentence_builder(quantity, tech_worker_type, countries, place, activity_list, morning):
